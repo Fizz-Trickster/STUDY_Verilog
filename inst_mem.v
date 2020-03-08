8594379c61197056448c90ad2cefa3e318208aed
fizz-trickster <yyd1109@gmail.com>
2020-03-08 17:17:31 +0900

    file name change

--- inst_mem.v
+++ inst_mem.v
@@ -1,12 +1,16 @@
-module InstructionMemory(
+module inst_mem(
+#(parameter DWIDTH = 8)
+
     input           i_mem_clk,
     input           i_rst_n,
     input   [31:0]  i_read_addr,
+    input   [31:0]  i_write_addr,
     output  [31:0]  o_instruction
 );
 
-    reg [7:0]   data [1023:0];
+    reg [DWIDTH-1:0]   data [1023:0];
 
+    // initialize
     initial begin
         $readmemh("vectors.txt", data);
     end
